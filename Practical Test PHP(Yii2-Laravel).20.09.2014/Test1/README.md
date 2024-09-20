deae70b88acb1539c3abe841737e6d3cbf3636a4
i-am-ivan <jovicorp.studio@gmail.com>
2024-09-20 08:23:14 +0300

    Beginning test 1

---
+++ Practical Test PHP(Yii2-Laravel).20.09.2014/Test1/README.md
@@ -0,0 +1,4 @@
+# Test 1: PHP Array Manipulation
+
+## Description: An array function to iterate 20 records in batches of 5 and print their index number and value  of each record.
+
---
+++ Practical Test PHP(Yii2-Laravel).20.09.2014/Test1/test1.php
@@ -0,0 +1,13 @@
+<?php
+# Test 1: PHP Array Manipulation
+
+# Description: An array function to iterate 20 records in batches of 5 and print their index number and value  of each record.
+
+# function to iterate through each array then print their index and value
+function print_array($array) {}
+
+function populate_array($record){
+    # population the array with 20 records
+}
+# print to console
+print(print_array($my_array));
