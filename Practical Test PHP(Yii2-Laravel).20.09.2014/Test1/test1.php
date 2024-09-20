2bec5c42887c2001264c9090218e5fd9cb61dd71
i-am-ivan <jovicorp.studio@gmail.com>
2024-09-20 08:42:10 +0300

    Test 1 Algorithm Test 1 success

--- Practical Test PHP(Yii2-Laravel).20.09.2014/Test1/test1.php
+++ Practical Test PHP(Yii2-Laravel).20.09.2014/Test1/test1.php
@@ -12,7 +12,20 @@ function populate_array($record){
 }
 
 # function to iterate through each array then print their index and value
-function print_array($array) {}
+function print_array($array) {
+    # Define the local batch size
+    $my_batch_size = 5;
+    # Iterate array in batches of 5
+    $my_batches = array_chunk($array, $my_batch_size); # Check array then in what batches.
+    # iterate through each records no matter what. Reason for foeach is it will go through everything all records in the array, better in this instance than using a for loop
+    foreach($my_batches as $my_batch){
+        foreach ($my_batch as $record_index => $record_value){
+            # Print to screen the index and records value side by side
+            echo "\n Index: $record_index, Value: $record_value";
+        }
+        echo "\n --------------------------------------------------------------- \n ";
+    }
+}
 
 # print to console
 $my_array = populate_array(20); # Populate the array with 20 records
