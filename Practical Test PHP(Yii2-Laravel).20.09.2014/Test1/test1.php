53ae3a8595a3aa2d8d71c8821c4c1d722e385d75
i-am-ivan <jovicorp.studio@gmail.com>
2024-09-20 08:31:23 +0300

    Test 1 algorithm complete

--- Practical Test PHP(Yii2-Laravel).20.09.2014/Test1/test1.php
+++ Practical Test PHP(Yii2-Laravel).20.09.2014/Test1/test1.php
@@ -3,11 +3,17 @@
 
 # Description: An array function to iterate 20 records in batches of 5 and print their index number and value  of each record.
 
+function populate_array($record){
+    # population the array with 20 records
+    $my_array = array(); # Initialize the array
+    for($r = 0; $r < $record; $r++){
+        $my_array[] = "Record_" . ($r + 1);
+    }
+}
+
 # function to iterate through each array then print their index and value
 function print_array($array) {}
 
-function populate_array($record){
-    # population the array with 20 records
-}
 # print to console
-print(print_array($my_array));
+$my_array = populate_array(20); # Populate the array with 20 records
+print($my_array); # Display array records to console
